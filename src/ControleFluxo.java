d629fa460d4103fae33266bba8462342435f277b
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-08-19 22:56:35 -0300

    controle de fluxo

---
+++ src/ControleFluxo.java
@@ -0,0 +1,15 @@
+public class ControleFluxo {
+    public static void main(String[] args) {
+        int idade = 15;
+        float salario = 2000;
+        boolean c = false;
+
+        if(idade>=18) {
+            System.out.println("Entrou no clube");
+        }else{
+            System.out.println("Não pode entrar no clube");
+        }
+        
+    }
+    
+}

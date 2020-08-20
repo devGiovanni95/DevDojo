2e5cb9dbdd2fc69d50f78298fa9b4f74996b5929
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-08-19 23:04:29 -0300

    Update ControleFluxo1.java

--- src/ControleFluxo1.java
+++ src/ControleFluxo1.java
@@ -5,13 +5,28 @@ public class ControleFluxo1 {
         //idade >= 18 adulto
         int idade = 15;
 
+        //pra imprimir na tela
+
+        // if(idade < 15) {
+        //     System.out.println("Categoria infantil");
+        // }else if(idade >= 15 && idade < 18){
+        //     System.out.println("Categoria juvenil");
+        // }else{
+        //     System.out.println("Categoria adulta");
+        // }
+
+      
+        String categoria;   //armazenar no sistema
+
+        
         if(idade < 15) {
-            System.out.println("Categoria infantil");
+            categoria = "Infantil";
         }else if(idade >= 15 && idade < 18){
-            System.out.println("Categoria juvenil");
+            categoria = "Juvenil";
         }else{
-            System.out.println("Categoria adulta");
+            categoria = "Adulto";
         }
+        System.out.println(categoria);
 
     }
 }

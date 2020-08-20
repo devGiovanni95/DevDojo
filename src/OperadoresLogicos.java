cd61f3f8c85bfa4317ce5ed4906d3b9924ff1fe7
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2020-08-19 21:58:17 -0300

    Add files via upload

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,9 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int idade = 18;
+        float salario = 1000f;
+
+        System.out.println(idade >= 18 && salario >= 2000);  //somente torna verdadeira quando as duas opções são verdadeiras
+        System.out.println(idade >= 18 || salario >= 2000);  //tendo apenas uma verdadeira ele aceita a comparação como verdadeira
+    }
+}

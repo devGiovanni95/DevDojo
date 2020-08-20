2e73190a00fb9e983fe644404daeb95889fc5cc6
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-08-19 22:41:32 -0300

    Create OperadoresDeAtribuicao.java

---
+++ src/OperadoresDeAtribuicao.java
@@ -0,0 +1,18 @@
+public class OperadoresDeAtribuicao {
+    public static void main (String[] args){
+        // =, -=, +=, *=, /=, %=
+        int salario = 1800;
+        
+        //salario = salario + 1000;    jeito mais extenso de se programar 
+        
+       // salario += 1000;  //jeito mais simples de se programar simplificado
+       // salario *= 0.1; // 1800 * 0.1 = 180 calculou no caso 10%
+       //salario = salario + (int) (salario * 0.1); //calcula o salario mais 10 %
+       // System.out.println(salario);
+
+       int numero = 11;
+       numero %=2;
+       System.out.println(numero);
+    }
+    
+}

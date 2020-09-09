d88489df33958f65f13eb01a11aa77da91e5f784
devGiovanni95 <giovanni.devjava@hotmail.com>
2020-09-08 21:35:33 -0300

    metodos

--- src/br.com.abc/javascore/introducaometodos/classes/Calculadora.java
+++ src/br.com.abc/javascore/introducaometodos/classes/Calculadora.java
@@ -4,7 +4,51 @@ public class Calculadora {
     public void somaDoisNumeros() {
         System.out.println(5+5);
     }
+
     public void subtraiDoisNumeros() {
-        System.out.println(5-5);
-    }    
+        System.out.println(5-5); 
+    }  
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1*num2);
+    }
+
+/*    public double divideDoisNumeros(double num1, double num2) {
+        //modo mais resumido para retornar o resultado da divisão
+        return num1/num2; //quando utilizamos o return necessitamos utilizar ele do mesmo tipo declarado nos metodos
+
+    //System.out.println(num1/num2 );  se quisermos mostrar na tela o resultado mas como não e o objetivo nao colocamos aqui
+
+
+      //forma mais completa de se retornar 2 numeros mas como so se esta utilizando para retornar o resultado da divisão tem um modo mais resumido
+        // double resultado = num1 / num2;
+       // return resultado;
+    }
+    */
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if(num2 != 0 ) {
+            return num1/num2;
+        }
+        return 0;
+    }
+
+    /* public void imprimeDoisNumerosDivididos(double num1, double num2) {
+        if(num2 != 0) {
+            System.out.println(num1/num2);
+        }else{
+            System.out.println("Não é possivel dividir por 0");
+        }
+    } 
+    */
+
+    public void imprimeDoisNumerosDivididos(double num1, double num2) {
+        if(num2 != 0) {
+            System.out.println(num1/num2);
+            return   //para quebrar o laço de repetição podemos utilizar o return quando estamos utilizando o if para não precisarmos utilizar o else
+            //ele é como se fosse o break do laço de retpetição 
+        }
+            System.out.println("Não é possivel dividir por 0");
+        
+    }
 }
--- src/br.com.abc/javascore/introducaometodos/test/CalculadoraTest.java
+++ src/br.com.abc/javascore/introducaometodos/test/CalculadoraTest.java
@@ -7,6 +7,18 @@ public class CalculadoraTest{
     Calculadora calc = new Calculadora();
     calc.somaDoisNumeros();
     calc.subtraiDoisNumeros();
+    calc.multiplicaDoisNumeros(5,5); //forçar um cast dentro do numero (5,(byte)5);
+    //pode se usar um numero inteiro com double (5d,5) ou (5.5,5)
+
+    // calc.divideDoisNumeros(20,2); para chamar a operação para o codigo
+
+    //para imprimir na tela podemos chamar da seguinte forma 
+    // System.out.println(calc.divideDoisNumeros(20,2));
+
+    //para chamarmos dentro de uma variavel posemos fazer da seguinte forma:
+     double result = calc.divideDoisNumeros(20,2); //se dividirmos algum numero por 0 teremos a mensagem infinity pois não existe
+     System.out.println(result);   
+
 
     System.out.println("Continue a operação");
 
